5fc3ae5e8b5e24fd63237bbd0fc4759f3ce7b788
xz <xz-jasper@qq.com>
2025-06-04 10:17:22 +0800

    init

--- src/main/java/org/example/GraalvmDemoApplication.java
+++ src/main/java/org/example/GraalvmDemoApplication.java
@@ -1,19 +1,21 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GraalvmDemoApplication {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SpringApplication.run(GraalvmDemoApplication.class, args);
+    }
+}
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, GraalVM!";
     }
 }
